414394716d1cd105b96c329391210e52ad3a1435
Nami <namitech3@gmail.com>
2022-07-29 20:43:22 +0900

    feat: add Header and routing

---
+++ components/common/BaseLayout.tsx
@@ -0,0 +1,26 @@
+import Header from './Header';
+import styled from '@emotion/styled';
+
+type BaseLayoutProps = {
+  children: React.ReactNode;
+};
+
+const BaseLayout = ({ children }: BaseLayoutProps) => {
+  return (
+    <Container>
+      <Header />
+      <ChildrenWrapper>{children}</ChildrenWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div<{ paddingBottom?: string }>`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding-bottom: ${({ paddingBottom }) => paddingBottom || 0};
+`;
+const ChildrenWrapper = styled.div`
+  flex-grow: 1;
+`;
+export default BaseLayout;
