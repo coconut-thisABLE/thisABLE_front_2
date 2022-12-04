dd8153b7664d373aad4754208bfe4528b0d7049c
Nami <namitech3@gmail.com>
2022-12-04 23:38:44 +0900

    refactor: use api client

--- api/usePlaceListQuery.ts
+++ api/usePlaceListQuery.ts
@@ -2,10 +2,15 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { PlaceListType } from '../types'
 
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: false,
+})
+
 //TODO: change fixed latitude and longitude
 const fetchPlaces = async (pageNumber: number): Promise<PlaceListType> => {
-  const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/?latitude=37.5441270&longitude=126.9667812&page=${pageNumber}`
+  const response = await apiClient.get(
+      `/?latitude=37.5441270&longitude=126.9667812&page=${pageNumber}`
   )
   if (response.status !== 200) {
     throw new Error('Unable to get place list')
